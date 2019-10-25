05d3a242ba1fbe475e74ac2d2ab4f1e482fb0572
Zixuan Guo <i@zxguo.me>
2019-10-25 13:40:59 -0500

    implement uint16 depth image reading

--- model/data.py
+++ model/data.py
@@ -1,28 +1,29 @@
 import torch
 import os
-from skimage import io
-from skimage.transform import resize
 from PIL import Image
 import torchvision.transforms as transforms
 
 
 class DepthDataset(torch.utils.data.Dataset):
-    def __init__(self):
-        self.path = "data.nosync/"
-        self.lst = [f for f in sorted(os.listdir(self.path)) if "r-" in f]
+    def __init__(self, path):
+        self.path = path
+        self.lst = [f for f in sorted(os.listdir(self.path)) if "rgb" in f]
 
     def __getitem__(self, index):
         # load image
         image = Image.open(os.path.join(self.path, self.lst[index]))
-        depth = resize(io.imread(os.path.join(self.path, self.lst[index]).replace("r-", "d-")), (62, 78))
+        depth = Image.open(os.path.join(self.path, self.lst[index].replace("rgb", "depth")))
 
         # transformation
         image_trans = transforms.Compose([
             transforms.Resize((240, 320)),
             transforms.ToTensor(),
+            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
         depth_trans = transforms.Compose([
-            transforms.ToTensor()
+            transforms.Resize((62, 78)),
+            transforms.ToTensor(),
+            #transforms.Normalize((0.5,), (0.5,))
         ])
         image = image_trans(image)
         depth = depth_trans(depth)
@@ -31,4 +32,3 @@ class DepthDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.lst)
-

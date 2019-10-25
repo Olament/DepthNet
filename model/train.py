c77ec6b71f331d5e5524343fdb2d0586a48023dc
Zixuan Guo <i@zxguo.me>
2019-10-25 13:41:28 -0500

    working prototype

--- model/train.py
+++ model/train.py
@@ -1,19 +1,17 @@
 import torch
-import torch.nn as nn
 
-import model
-import data
-import losses
+from model import model, losses, data
 
 # hyperparameter
 batch_size = 32
 learning_rate = 0.001
+total_epoch = 20
 
 # Device configuration
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
 #Datasets and loader
-dataset = data.DepthDataset()
+dataset = data.DepthDataset("data/")
 
 loader = torch.utils.data.DataLoader(dataset=dataset,
                                      batch_size=batch_size,
@@ -28,19 +26,20 @@ optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
 # train
 total_step = len(loader)
-for i, (image, depth) in enumerate(loader):
-    image = image.to(device)
-    depth = depth.to(device)
+for j in range(total_epoch):
+    for i, (image, depth) in enumerate(loader):
+        image = image.to(device)
+        depth = depth.to(device)
 
-    # forward pass
-    outputs = model(image)
-    loss = criterion(outputs, depth)
+        # forward pass
+        outputs = model(image)
+        loss = criterion(outputs, depth)
 
-    # Backward and optimize
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        # Backward and optimize
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    if (i + 1) % 10 == 0:
-        print("Step [{}/{}] Loss: {:.4f}"
-              .format(i, total_step, loss.item()))
+        if (i + 1) % 10 == 0:
+            print("Epoch: [{}/{}] Step [{}/{}] Loss: {:.4f}"
+                  .format((j+1), total_epoch, i, total_step, loss.item()))

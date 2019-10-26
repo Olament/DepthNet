692fedad6d36b36a77c38d10db519af04421f8bd
Zixuan Guo <i@zxguo.me>
2019-10-26 16:29:07 -0500

    add centercrop and flip transformation

--- model/data.py
+++ model/data.py
@@ -6,17 +6,24 @@ import torchvision.transforms as transforms
 
 class DepthDataset(torch.utils.data.Dataset):
     def __init__(self, path):
-        self.path = path
-        self.lst = [f for f in sorted(os.listdir(self.path)) if "rgb" in f]
+        self.lst = []
+        for r, d, f in os.walk(path):
+            for file in f:
+                if 'rgb' in file:
+                    self.lst.append(os.path.join(r, file))
 
     def __getitem__(self, index):
         # load image
-        image = Image.open(os.path.join(self.path, self.lst[index]))
-        depth = Image.open(os.path.join(self.path, self.lst[index].replace("rgb", "depth")))
+        image = Image.open(self.lst[index])
+        depth = Image.open(self.lst[index].replace("rgb", "depth"))
 
         # transformation
-        image_trans = transforms.Compose([
+        comm_trans = transforms.Compose([
             transforms.Resize((240, 320)),
+            transforms.CenterCrop((228, 304)),
+            transforms.RandomHorizontalFlip()
+        ])
+        image_trans = transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
         ])
@@ -24,10 +31,10 @@ class DepthDataset(torch.utils.data.Dataset):
             transforms.Resize((64, 80)),
             transforms.ToTensor(),
             transforms.Lambda(lambda x: x.float()),
-            transforms.Lambda(lambda x:  x / 65535.0)
+            #transforms.Lambda(lambda x:  torch.div(x, 65535.0))
         ])
-        image = image_trans(image)
-        depth = depth_trans(depth)
+        image = image_trans(comm_trans(image))
+        depth = depth_trans(comm_trans(depth))
 
         return image, depth
 

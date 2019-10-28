fbbcfb1a802d9ef81fe379e5778cca51cb132d4a
Zixuan Guo <i@zxguo.me>
2019-10-28 00:29:13 -0500

    add bn to upsampling block and relu to depthnet

--- model/depthnet.py
+++ model/depthnet.py
@@ -21,8 +21,10 @@ class UpSamplingBlock(nn.Module):
         self.unpool = nn.MaxUnpool2d(2, stride=2)
         self.pool = nn.MaxPool2d(2, stride=2, return_indices=True)
         self.conv1 = conv5x5(in_channels, out_channels)
+        self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
         self.conv2 = conv3x3(out_channels, out_channels)
+        self.bn2 = nn.BatchNorm2d(out_channels)
 
     def forward(self, x):
         # create indices for unpool
@@ -31,10 +33,13 @@ class UpSamplingBlock(nn.Module):
         # unpool and assign residual
         out = self.unpool(x, indices.to(device))
         residual = self.conv1(out)
+        residual = self.bn1(residual)
         # forward and projection
         out = self.conv1(out)
+        out = self.bn1(residual)
         out = self.relu(out)
         out = self.conv2(out)
+        out = self.bn2(residual)
         out += residual
         return out
 
@@ -43,7 +48,7 @@ class UpSamplingBlock(nn.Module):
 class DepthNet(nn.Module):
     def __init__(self):
         super(DepthNet, self).__init__()
-        # Remove FC and AvgPool layer from Resnet34
+        # Remove FC and AvgPool layer from Resnet50
         resnet = torchvision.models.resnet50(pretrained=True)
         modules = list(resnet.children())[:-2]
         self.resnet = nn.Sequential(*modules)
@@ -51,8 +56,11 @@ class DepthNet(nn.Module):
         self.bn = nn.BatchNorm2d(1024)
         # Add new upsampling layer
         self.up1 = nn.Sequential(UpSamplingBlock(1024, 512),
+                                 nn.ReLU(),
                                  UpSamplingBlock(512, 256),
-                                 UpSamplingBlock(256, 128))
+                                 nn.ReLU(),
+                                 UpSamplingBlock(256, 128),
+                                 nn.ReLU())
         self.conv2 = conv3x3(128, 1)
         self.relu = nn.ReLU(inplace=True)
 
